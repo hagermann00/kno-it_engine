1453068ad9b2ea19be8d55375fccae37f8c3e2c8
hagermann00 <BRIHAG8@gmail.com>
2025-06-19 17:22:31 -0500

    eeend point 1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.get('/hello', (req, res) => res.send('Hello Kno-It!'));
 
 app.get('/', (req, res) => {
   res.send('Kno-It backend is running!');
