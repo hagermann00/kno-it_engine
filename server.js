cd205b66441f0f4d550bda2e53fb2aaf67f36254
hagermann00 <BRIHAG8@gmail.com>
2025-06-21 01:20:40 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.get('/hello', (req, res) => res.send('Hello Kno-It!'));
 
 app.get('/', (req, res) => {
